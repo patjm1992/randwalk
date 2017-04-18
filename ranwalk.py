d5c033bf7ae76298d0b5d316bbf0eb0383fb2476
patjm1992 <patjm1992@gmail.com>
2017-04-18 16:40:31 -0400

    working

--- ranwalk.py
+++ ranwalk.py
@@ -15,22 +15,55 @@ import math
 	Use a 100 x 100 grid with the walker beginning in the middle.
 '''
 
-height = 500
-width = 500
+height = 200
+width = 200
 
-
+def main():
+	window, center = gui()
+	print("Walker is at: ", center)
+	while True:
+		steps = eval(input("How many steps do you want to take? "))
+		ran_walk(steps, window, center)
+		print("Walker is at ")
 
 def gui():
 	win = GraphWin("Let's take a walk!", height, width)
 	win.setBackground("White")
 	center = Point(height / 2, width / 2)
 	center.draw(win)
+	return win, center
 
 def generate_direction():
 	return random() * 2 * math.pi
 
+def new_point(direction):
+	pass
 
-# Text user interface prototype
-gui()
 
-time.sleep(10)
+def ran_walk(steps, win, walker):
+
+	# Center
+	x = y = height / 2
+
+	for i in range(steps):
+		angle = generate_direction()
+
+		x_change = math.cos(angle)
+		y_change = math.sin(angle)
+
+		# this moves the walker, but doesn't draw it yet
+		walker.move(x_change, y_change)
+
+		new_x = x + x_change
+		new_y = y + y_change
+
+		new_line = Line(Point(x, y), Point(new_x, new_y))
+		new_line.draw(win)
+
+		# 'Save' a new x and y position
+		x = new_x
+		y = new_y
+
+
+
+if __name__ == '__main__': main()
